d04fd8541cfc21d65981ba3ca18e7adaa6edf6f5
Nick Burridge <nick@halcyondigital.co.nz>
2021-02-17 10:20:07 +1300

    Basic dragging test

--- js/asset-tracker.js
+++ js/asset-tracker.js
@@ -3,14 +3,23 @@
   
     // Put here your custom JavaScript code.
     console.log ("Hello World");
-  })();
+
+
+
+})();
   
-/*(($, Drupal, drupalSettings) => {
+(($, Drupal, drupalSettings) => {
     Drupal.behaviors.assetTracker = {
       attach: function attach() {
         const path = drupalSettings.path.currentPath;
         alert("hello");
+        var el = $('.view-asset-list').find('.view-content');
+        $(el).each(function (i,e) {
+            var sortable = Sortable.create(e, {
+                animation: 150,
+                handle: '.drag-handle'
+            });
+        })
       }
     }
 })(jQuery, Drupal, drupalSettings);
-  */
