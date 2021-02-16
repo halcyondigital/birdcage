456a748e555a83af3016232406c281efed038171
Nick Burridge <nick@halcyondigital.co.nz>
2021-02-17 09:56:30 +1300

    Asset tracker js test

--- css/asset-tracker.js
+++ css/asset-tracker.js
@@ -1,4 +1,11 @@
-(($, Drupal, drupalSettings) => {
+(function () {
+    'use strict';
+  
+    // Put here your custom JavaScript code.
+    console.log ("Hello World");
+  })();
+  
+/*(($, Drupal, drupalSettings) => {
     Drupal.behaviors.assetTracker = {
       attach: function attach() {
         const path = drupalSettings.path.currentPath;
@@ -6,4 +13,4 @@
       }
     }
 })(jQuery, Drupal, drupalSettings);
-  
+  */
