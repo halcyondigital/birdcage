66a6e17ebd8b5c172d20643dbb12b237141c13fb
Nick Burridge <nick@halcyondigital.co.nz>
2021-02-17 13:41:16 +1300

    Pull UUID out of data attribute

--- js/asset-tracker.js
+++ js/asset-tracker.js
@@ -21,14 +21,14 @@
                 onAdd: function (/**Event*/evt) {
                   var assetEl = evt.item;  // dragged HTMLElement
                   var locationEl = evt.to;    // target list
-                  var assetID = $(assetEl).find('.asset-uuid').text();
-                  //var locationID = $(locationEl).parents('.location');
-                  console.log(locationEl);
-                  var locationID = "8bc52bfd-b1ea-4d04-b7a9-e79337aa6703";
+                  var assetId = $(assetEl).find('.asset-uuid').text();
+                  var locationId = $(locationEl).parents('.location').data('locationUuid');
+                  console.log(locationId);
+                  //var locationID = "8bc52bfd-b1ea-4d04-b7a9-e79337aa6703";
                   var asset = {
                     data: {
                       type: "node--asset",
-                      id: assetID, // eg "d4fac94c-b6e1-4f99-8258-81fa6ec7e605"
+                      id: assetId, // eg "d4fac94c-b6e1-4f99-8258-81fa6ec7e605"
                       attributes: {
                         title: "API test HDD"
                       },
@@ -36,14 +36,14 @@
                         field_location: {
                           data: {
                             type: "node--location",
-                            id: locationID // eg "8bc52bfd-b1ea-4d04-b7a9-e79337aa6703"
+                            id: locationId // eg "8bc52bfd-b1ea-4d04-b7a9-e79337aa6703"
                           }
                         }
                       }
                     }
                   };
                   getCsrfToken(function (csrfToken) {
-                    updateAsset(csrfToken, asset, assetID);
+                    updateAsset(csrfToken, asset, assetId);
                   });
                 },
             });
@@ -57,9 +57,9 @@
               });
         }
         
-        function updateAsset(csrfToken, asset, assetID) {
+        function updateAsset(csrfToken, asset, assetId) {
           $.ajax({
-              url: `/jsonapi/node/asset/${assetID}`,
+              url: `/jsonapi/node/asset/${assetId}`,
               method: 'PATCH',
               headers: {
                   'Content-Type': 'application/vnd.api+json',
